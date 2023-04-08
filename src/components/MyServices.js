4ee80f31e16ab74d1d28f754e88fae7c5aff05d4
sooto selim <adewaleselim6@gmail.com>
2023-04-08 14:18:20 +0100

    working on fonts

--- src/components/MyServices.js
+++ src/components/MyServices.js
@@ -1,9 +1,35 @@
 const MyServices = () => {
   return (
     <div>
-      <div className="container bg-red-400">
-        <div>
-          <h2>VIDEO TV AND FILM PRODUCTION</h2>
+      <div className="container md:h-[80vh] flex justify-center items-center">
+        <div className="flex items-center gap-x-4">
+          {/* first card */}
+          <div className="flexCenter flex-col gap-y-4">
+            <div className="w-20 h-1 bg-purple-500"></div>
+            <h2 className=" font-bold font">VIDEO TV AND FILM PRODUCTION</h2>
+            <p className="text-center">
+              We work with professional quality camera equipment, such as our
+              Black Magic and Canon 5d MK 3 cameras, used on Hollywood films.
+            </p>
+          </div>
+          {/* first card */}
+          <div className="flexCenter flex-col gap-y-4">
+            <div className="w-20 h-1 bg-purple-500"></div>
+            <h2 className=" font-bold font">VIDEO TV AND FILM PRODUCTION</h2>
+            <p className="text-center">
+              We work with professional quality camera equipment, such as our
+              Black Magic and Canon 5d MK 3 cameras, used on Hollywood films.
+            </p>
+          </div>
+          {/* first card */}
+          <div className="flexCenter flex-col gap-y-4">
+            <div className="w-20 h-1 bg-purple-500"></div>
+            <h2 className=" font-bold font">VIDEO TV AND FILM PRODUCTION</h2>
+            <p className="text-center">
+              We work with professional quality camera equipment, such as our
+              Black Magic and Canon 5d MK 3 cameras, used on Hollywood films.
+            </p>
+          </div>
         </div>
       </div>
     </div>
