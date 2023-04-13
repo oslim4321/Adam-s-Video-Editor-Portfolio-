3a586ef489c2b6eb8ac9b5abbac143638ea30b81
sooto selim <adewaleselim6@gmail.com>
2023-04-13 13:46:28 +0100

    working on the popup asing user to cinfirm mobile screen

--- src/components/DesktopViewButton .js
+++ src/components/DesktopViewButton .js
@@ -1,25 +1,75 @@
-import React from "react";
+// import React, { useEffect } from "react";
 
-const DesktopViewButton = () => {
-  const handleClick = () => {
-    const userAgent = window.navigator.userAgent;
-    console.log(userAgent, "userAgent");
-    // Check if the user agent contains the keyword "Mobile"
-    const isMobile = /Mobile/.test(userAgent);
-    console.log(isMobile, "isMobile");
-    // If the user agent indicates a mobile device
-    if (isMobile) {
-      // Change the viewport meta tag to disable responsiveness
-      const metaViewport = document.querySelector('meta[name="viewport"]');
-      metaViewport.content = "width=1200, initial-scale=1.0";
-    }
+// const DesktopViewAlert = () => {
+//   const userAgent = window.navigator.userAgent;
+//   const isMobile = /Mobile/.test(userAgent);
+//   const showDesktopViewAlert = () => {
+//     // Show the alert after a delay of 4 seconds (4000 milliseconds)
+//     setTimeout(() => {
+//       const result = window.confirm(
+//         "Would you like to switch to desktop view?"
+//       );
+//       if (result) {
+//         // Change the viewport meta tag to disable responsiveness
+//         const metaViewport = document.querySelector('meta[name="viewport"]');
+//         metaViewport.content = "width=1200, initial-scale=1.0";
+//       }
+//     }, 4000);
+//   };
+
+//   useEffect(() => {
+//     // Call the function to show the alert when the component mounts it only show if isMobile is true
+//     if (isMobile) {
+//       showDesktopViewAlert();
+//     }
+//   }, []);
+
+//   return (
+//     // Render your component UI here
+//     <div>
+//       <p>Welcome to my website!</p>
+//       {/* Render other content */}
+//     </div>
+//   );
+// };
+
+// export default DesktopViewAlert;
+
+import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const DesktopViewAlert = () => {
+  const showDesktopViewAlert = () => {
+    // Show the alert after a delay of 4 seconds (4000 milliseconds)
+    setTimeout(() => {
+      const result = window.confirm(
+        "Would you like to switch to desktop view?"
+      );
+      if (result) {
+        // Change the viewport meta tag to disable responsiveness
+        const metaViewport = document.querySelector('meta[name="viewport"]');
+        metaViewport.content = "width=1200, initial-scale=1.0";
+      }
+    }, 4000);
   };
 
+  useEffect(() => {
+    // Call the function to show the alert when the component mounts, but only on mobile devices
+    const isMobile = typeof window !== "undefined" && window.innerWidth < 768; // Set the desired breakpoint for mobile devices
+    if (isMobile) {
+      showDesktopViewAlert();
+    }
+  }, []);
+
   return (
-    <section>
-      <button onClick={handleClick}>Switch to Desktop View</button>
-    </section>
+    // Render your component UI here
+    <div>
+      <p>Welcome to my website!</p>
+      {/* Render other content */}
+    </div>
   );
 };
 
-export default DesktopViewButton;
+export default dynamic(() => Promise.resolve(DesktopViewAlert), {
+  ssr: false, // Set ssr to false to avoid server-side rendering errors
+});
