3bd7b5af5ea95b36d8b3addea8b35c95b0332d5b
sooto selim <adewaleselim6@gmail.com>
2023-04-29 12:27:37 +0100

    working

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -15,29 +15,29 @@ const Testimonial = () => {
             decision I am glad I made, it has improved the delivery of my Brand
             over time. Your services are top-notch and satisfactory. Thank you!
           </p>
-          <img
+          {/* <img
             src="/images/mt-1384-home-signature.png"
             alt=""
             className="w-20"
-          />
-          <h3 className="text-2xl text-white">LINDA DYLLAN</h3>
+          /> */}
+          <h3 className="text-2xl text-white text-right">BYGDONALD FILMS</h3>
           <p className="text-white">Clients</p>
         </div>
         {/* image */}
-        <div className="w-[100%]">
-          <Image
+        {/* <div className="w-[100%]"> */}
+        {/* <Image
             src="/images/pexels-fauxels-3184465.jpg"
             alt="our clients"
             width={400}
             height={400}
             className="w-full h-full object-cover gap-x-5"
-          />
-          {/* <img
+          /> */}
+        {/* <img
             className="w-full h-full object-cover gap-x-5"
             src="/images/pexels-fauxels-3184465.jpg"
             alt=""
           /> */}
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );
