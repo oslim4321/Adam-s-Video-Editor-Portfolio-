c98e5652448dae52a111ec037a58fa77606dd6a7
sooto selim <adewaleselim6@gmail.com>
2023-05-05 15:19:13 +0100

    working on fetching youtube video channel still working

---
+++ src/components/ExploreVideos.js
@@ -0,0 +1,113 @@
+import Aside from "@/components/Aside";
+import React from "react";
+
+const ExploreVideos = ({ videoItems, onVideoLoad }) => {
+  const products = [
+    { id: 1, name: "Product 1", description: "Description for product 1" },
+    { id: 2, name: "Product 2", description: "Description for product 2" },
+    { id: 3, name: "Product 3", description: "Description for product 3" },
+    { id: 4, name: "Product 4", description: "Description for product 4" },
+    { id: 5, name: "Product 5", description: "Description for product 5" },
+    { id: 6, name: "Product 6", description: "Description for product 6" },
+    { id: 7, name: "Product 7", description: "Description for product 7" },
+    { id: 8, name: "Product 8", description: "Description for product 8" },
+  ];
+  return (
+    <>
+      {/* <button
+        data-drawer-target="default-sidebar"
+        data-drawer-toggle="default-sidebar"
+        aria-controls="default-sidebar"
+        type="button"
+        class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span class="sr-only">Open sidebar</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </button> */}
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-2">
+          <Aside />
+        </div>
+        <div className="p-1 max-w-[95%] mx-auto w-full col-span-10">
+          <div className="flex md:grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-scroll md:overflow-hidden">
+            {/* <div className="bg-white md:w-full shadow rounded-lg">
+              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
+              <div className="p-4">
+                <h3 className="font-medium text-gray-900">Product 1</h3>
+                <p className="text-gray-500 mt-2">
+                  Description of product 1 goes here.
+                </p>
+                <p className="text-gray-900 font-medium mt-2">$19.99</p>
+              </div>
+            </div> */}
+
+            {/* <div className="bg-white md:w-full shadow rounded-lg">
+              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
+              <div className="p-4">
+                <h3 className="font-medium text-gray-900">Product 2</h3>
+                <p className="text-gray-500 mt-2">
+                  Description of product 2 goes here.
+                </p>
+                <p className="text-gray-900 font-medium mt-2">$29.99</p>
+              </div>
+            </div> */}
+
+            {videoItems.map((item) => (
+              // <div key={item.id} className="w-full">
+              //   <iframe
+              //     className="w-full "
+              //     // width="100"
+              //     // height="315"
+              //     src={`https://www.youtube.com/embed/${item.snippet.resourceId.videoId}`}
+              //     title={item.snippet.title}
+              //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              //     allowFullScreen
+              //   ></iframe>
+              //   <h2>{item.snippet.title}</h2>
+              //   <p>{item.snippet.description}</p>
+              // </div>
+
+              <div
+                key={item.id}
+                className="bg-white md:w-full shadow rounded-lg"
+              >
+                <div className="bg-gray-200 h-64 md:w-full w-[250px]">
+                  <iframe
+                    className="w-full h-full object-cover "
+                    // width="100"
+                    // height="315"
+                    src={`https://www.youtube.com/embed/${item.snippet.resourceId.videoId}`}
+                    title={item.snippet.title}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    onLoad={onVideoLoad}
+                  ></iframe>
+                </div>
+                <div className="p-4">
+                  <h2>{item.snippet.title}</h2>
+                  <p>{item.snippet.description.slice(0, 150)}...</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* <!-- Repeat this HTML for each product --> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExploreVideos;
--- src/pages/exploreVideos.js
+++ src/pages/exploreVideos.js
@@ -1,99 +1,58 @@
-import Aside from "@/components/Aside";
-import VideoPlayer from "@/components/VideoPlayer";
+// import VideoPlayer from "@/components/VideoPlayer";
+// import React from "react";
+// // cnannell id to generate different playlist
+// /*     // "https://youtube.googleapis.com/youtube/v3/playlists?channelId=UCRlV33-dIWOkQlex3XkC7Dw&key=AIzaSyAXcq1J0MTDhZjg8Cw2oKi0iZAIg8UOOGI"
+//  */
+
+// export async function getServerSideProps(context) {
+//   // Fetch data from an API
+//   const res = await fetch(process.env.youtubeurl);
+//   const data = await res.json();
+
+//   // Pass data to the page component as props
+//   return {
+//     props: { data },
+//   };
+// }
+
+// function MyPage({ data }) {
+//   const { items } = data;
+
+//   // Use the data to render the page
+//   return (
+//     <>
+//       <h1>Video player:</h1>
+//       <VideoPlayer videoId={videoId} />
+//     </>
+//   );
+// }
+
+// export default MyPage;
+
+import ExploreVideos from "@/components/ExploreVideos";
 import React from "react";
 
-const exploreVideos = () => {
-  const products = [
-    { id: 1, name: "Product 1", description: "Description for product 1" },
-    { id: 2, name: "Product 2", description: "Description for product 2" },
-    { id: 3, name: "Product 3", description: "Description for product 3" },
-    { id: 4, name: "Product 4", description: "Description for product 4" },
-    { id: 5, name: "Product 5", description: "Description for product 5" },
-    { id: 6, name: "Product 6", description: "Description for product 6" },
-    { id: 7, name: "Product 7", description: "Description for product 7" },
-    { id: 8, name: "Product 8", description: "Description for product 8" },
-  ];
+export async function getServerSideProps(context) {
+  // Fetch data from an API
+  const res = await fetch(process.env.youtubeurl);
+  const data = await res.json();
+
+  // Extract video items
+  const videoItems = data.items;
+
+  // Pass video items to the page component as props
+  return {
+    props: { videoItems },
+  };
+}
+
+function VideoList({ videoItems }) {
+  console.log(videoItems);
   return (
-    <>
-      {/* <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      >
-        <span class="sr-only">Open sidebar</span>
-        <svg
-          class="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          ></path>
-        </svg>
-      </button> */}
-      <div className="grid grid-cols-12 ">
-        <div className="col-span-2">
-          <Aside />
-        </div>
-        <div className="p-1 max-w-[95%] mx-auto w-full col-span-10">
-          <div className="flex md:grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-scroll md:overflow-hidden">
-            {/* <div className="bg-white md:w-full shadow rounded-lg">
-              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
-              <div className="p-4">
-                <h3 className="font-medium text-gray-900">Product 1</h3>
-                <p className="text-gray-500 mt-2">
-                  Description of product 1 goes here.
-                </p>
-                <p className="text-gray-900 font-medium mt-2">$19.99</p>
-              </div>
-            </div> */}
-
-            {/* <div className="bg-white md:w-full shadow rounded-lg">
-              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
-              <div className="p-4">
-                <h3 className="font-medium text-gray-900">Product 2</h3>
-                <p className="text-gray-500 mt-2">
-                  Description of product 2 goes here.
-                </p>
-                <p className="text-gray-900 font-medium mt-2">$29.99</p>
-              </div>
-            </div> */}
-
-            {/* <div className="bg-white md:w-full shadow rounded-lg">
-              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
-              <div className="p-4">
-                <h3 className="font-medium text-gray-900">Product 3</h3>
-                <p className="text-gray-500 mt-2">
-                  Description of product 3 goes here.
-                </p>
-                <p className="text-gray-900 font-medium mt-2">$39.99</p>
-              </div>
-            </div> */}
-
-            <div className="bg-white md:w-full shadow rounded-lg">
-              <div className="bg-gray-200 h-64 md:w-full w-[250px]"></div>
-              <div className="p-4">
-                {/* <h3 className="font-medium text-gray-900">Product 4</h3>
-                <p className="text-gray-500 mt-2">
-                  Description of product 4 goes here.
-                </p>
-                <p className="text-gray-900 font-medium mt-2">$49.99</p> */}
-                <VideoPlayer />
-              </div>
-            </div>
-          </div>
-
-          {/* <!-- Repeat this HTML for each product --> */}
-        </div>
-      </div>
-    </>
+    <div>
+      <ExploreVideos videoItems={videoItems} />
+    </div>
   );
-};
+}
 
-export default exploreVideos;
+export default VideoList;
