c2a1123e216b261b089717c25311b26d4f9616b0
sooto selim <adewaleselim6@gmail.com>
2023-05-05 18:55:46 +0100

    fixing a bug on production 4

--- src/pages/api/getVideos.js
+++ src/pages/api/getVideos.js
@@ -5,9 +5,10 @@ export default async function handler(req, res) {
   let apiUrl;
   if (playlistId.startsWith("PLA")) {
     console.log("i am running");
-    apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=10&key=PLAmG0Hd23HiJ3YeDhx5Yqdt--r998WcTO`;
+    apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=10&key=${process.env.YOUTUBE_API_KEY}`;
   } else {
-    apiUrl = process.env.NEXT_PUBLIC_YOUTUBEURL;
+    apiUrl =
+      apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=10&key=PLAmG0Hd23HiJ3YeDhx5Yqdt--r998WcTO`;
   }
   const response = await fetch(apiUrl);
   const data = await response.json();
