546b86d977ff532e7354de3dad779eed03b63ac1
sooto selim <adewaleselim6@gmail.com>
2023-05-08 13:54:43 +0100

    working here6

--- src/components/ExploreVideos.js
+++ src/components/ExploreVideos.js
@@ -28,9 +28,9 @@ const ExploreVideos = ({ videoItems, setplaylistId, loading }) => {
         data-drawer-target="default-sidebar"
         data-drawer-toggle="default-sidebar"
         aria-controls="default-sidebar"
-        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg"
       >
-        <span className="sr-only">Open sidebar</span>
+        {/* <span className="sr-only">Open sidebar</span>
         <svg
           className="w-6 h-6"
           aria-hidden="true"
@@ -43,7 +43,15 @@ const ExploreVideos = ({ videoItems, setplaylistId, loading }) => {
             fill-rule="evenodd"
             d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
           ></path>
-        </svg>
+        </svg> */}
+        {}
+        <div className="text-3xl">
+          {sideBar ? (
+            <i className="bi bi-x-circle-fill"></i>
+          ) : (
+            <i className="bi bi-list"></i>
+          )}
+        </div>
       </div>
       <div className="grid grid-cols-12 ">
         {sideBar ? (
